4c6beb2c0a607ba1957b73b9fb70343712e494dc
Javier <35864946+jmagit@users.noreply.github.com>
2025-12-03 16:01:31 +0100

    ejemplos

---
+++ tests/calculadora.spec.ts
@@ -0,0 +1,211 @@
+import { test, expect, Locator } from '@playwright/test';
+
+// test.use({ storageState: 'playwright/.auth/user-storage-state.json' });
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('/calculadora');
+})
+
+test('llega a la página calculadora', { tag: '@smoke' }, async ({ page }) => {
+  await expect(page).toHaveTitle('Calculadora')
+});
+
+test.describe('Botones de la calculadora', () => {
+  let pantalla: Locator
+
+  test.beforeEach(async ({ page }) => {
+    pantalla = page.locator('#txtPantalla')
+  })
+
+  // eslint-disable-next-line playwright/expect-expect
+  test('básico (sin aserciones directas)', async ({ page }) => {
+    await page.getByRole('button', { name: '9' }).click();
+    await page.getByRole('button', { name: '8' }).click();
+    await page.getByRole('button', { name: '7' }).click();
+    await page.getByRole('button', { name: '4' }).click();
+    await page.getByRole('button', { name: '5' }).click();
+    await page.getByRole('button', { name: '6' }).click();
+    await page.getByRole('button', { name: '3' }).click();
+    await page.getByRole('button', { name: '2' }).click();
+    await page.getByRole('button', { name: '1' }).click();
+    await page.getByRole('button', { name: '0' }).click();
+    await page.getByText('9874563210').click();
+  });
+
+  test('botones de la calculadora', async ({ page }) => {
+    await test.step('1 - botones numéricos', async () => {
+      await page.getByRole('button', { name: '9' }).click();
+      await page.getByRole('button', { name: '8' }).click();
+      await page.getByRole('button', { name: '7' }).click();
+      await page.getByRole('button', { name: '6' }).click();
+      await page.getByRole('button', { name: '5' }).click();
+      await page.getByRole('button', { name: '4' }).click();
+      await page.getByRole('button', { name: '3' }).click();
+      await page.getByRole('button', { name: '1' }).click();
+      await page.getByRole('button', { name: '0' }).click();
+      await page.getByRole('button', { name: ',' }).click();
+      await page.getByRole('button', { name: '1' }).click();
+      await expect(pantalla).toContainText('987654310,1');
+    });
+    await test.step('2 - cambio de signo', async () => {
+      await page.getByRole('button', { name: '±' }).click();
+      await expect(pantalla).toContainText('-987654310,1');
+    });
+    await test.step('3 - borrar uno a uno', async () => {
+      await page.getByRole('button', { name: '↶ BORRAR' }).click();
+      await page.getByRole('button', { name: '↶ BORRAR' }).click();
+      await page.getByRole('button', { name: '↶ BORRAR' }).click();
+      await page.getByRole('button', { name: '↶ BORRAR' }).click();
+      await expect(pantalla).toContainText('-9876543');
+    });
+    await test.step('4 - borrar todo', async () => {
+      await page.getByRole('button', { name: 'C' }).click();
+      await pantalla.click();
+      await expect(pantalla).toContainText('0');
+    });
+  });
+
+  test('bucle botones de la calculadora', async ({ page }) => {
+    const secuencia = '98765432,01'
+    for (let index = 0; index < secuencia.length; index++) {
+      await page.getByRole('button', { name: secuencia[index] as string }).click();
+      await expect(pantalla).toContainText(secuencia.substring(0, index + 1));
+    }
+    await page.getByRole('button', { name: '±' }).click();
+    await expect(pantalla).toHaveText(`-${secuencia}`);
+    await page.getByRole('button', { name: '±' }).click();
+    await expect(pantalla).toHaveText(secuencia);
+    for (let index = 1; index < 5; index++) {
+      await page.getByRole('button', { name: '↶ BORRAR' }).click();
+      await expect(pantalla).toHaveText(secuencia.substring(0, secuencia.length - index));
+    }
+    await page.getByRole('button', { name: 'C' }).click();
+    await expect(pantalla).toHaveText('0');
+    await page.getByRole('button', { name: '1' }).click();
+    await page.getByRole('button', { name: '±' }).click();
+    await expect(pantalla).toHaveText('-1');
+    await page.getByRole('button', { name: '↶ BORRAR' }).click();
+    await expect(pantalla).toHaveText('0');
+  });
+
+})
+
+test.describe('Operaciones de la calculadora', () => {
+  let pantalla: Locator
+  let resumen: Locator
+
+  test.beforeEach(async ({ page }) => {
+    pantalla = page.locator('#txtPantalla')
+    resumen = page.locator('#txtResumen')
+  })
+
+  test.describe('Botones de operaciones', () => {
+    [
+      { operacion: '+', resultado: '6' },
+      { operacion: '-', resultado: '0' },
+      { operacion: '*', resultado: '9' },
+      { operacion: '/', resultado: '1' },
+    ].forEach(async ({ operacion, resultado }) => {
+      test(`Botón ${operacion}`, async ({ page }) => {
+        await page.getByRole('button', { name: '3' }).click();
+        await page.getByRole('button', { name: operacion }).click();
+        await expect(pantalla).toHaveText('3');
+        await expect(resumen).toHaveText(`3 ${operacion}`);
+        await page.getByRole('button', { name: '=' }).click();
+        await expect(pantalla).toHaveText(resultado);
+        await expect(resumen).toBeHidden();
+      })
+    })
+  })
+
+  test('Error IEE-754', () => {
+    expect.soft(0.1 + 0.2).toBe(0.3)
+    expect.soft(1 - 0.9).toBe(0.1)
+    expect.soft(1/0).toBe(0)
+    expect.soft(1/0*0).toBe(0)
+    // test.info().expectedStatus = 'failed'
+  })
+
+  test.describe('Cálculos', () => {
+    [
+      { operacion: '4321-1234*666/30=', resultado: '68531,4' },
+      { operacion: '0,1+0,2=', resultado: '0,3' },
+      { operacion: '1-0,9=', resultado: '0,1' },
+      { operacion: '1/0=', resultado: 'Infinity' },
+      { operacion: '1-2/0=', resultado: '-Infinity' },
+      { operacion: '1/0*0=', resultado: 'NaN' },
+    ].forEach(async ({ operacion, resultado }) => {
+      test(`${operacion}=${resultado}`, async ({ page }) => {
+        for (const name of operacion) {
+          await page.getByRole('button', { name }).click();
+          if (name === "=") break;
+        }
+        await expect(pantalla).toHaveText(resultado);
+        await expect(resumen).toBeHidden();
+      })
+    })
+  })
+})
+
+test.describe('Instantáneas de la calculadora', () => {
+  const operaciones = [
+    { operacion: '+', nombre: 'sum', },
+    { operacion: '-', nombre: 'rest', },
+    { operacion: '*', nombre: 'multi', },
+    { operacion: '/', nombre: 'div', },
+  ]
+
+  test.describe('Con visual snapshot', () => {
+    operaciones.forEach(async ({ operacion, nombre }) => {
+      test(`visual snapshot ${nombre}`, async ({ page }) => {
+        await page.getByRole('button', { name: '5' }).click();
+        await page.getByRole('button', { name: operacion }).click();
+        await expect(page).toHaveScreenshot();
+      })
+    })
+  })
+
+  test.describe('Con content snapshot', () => {
+    operaciones.forEach(async ({ operacion, nombre }) => {
+      test(`content snapshot ${nombre}`, async ({ page }) => {
+        await page.getByRole('button', { name: '5' }).click();
+        await page.getByRole('button', { name: operacion }).click();
+        expect(await page.innerHTML('main')).toMatchSnapshot();
+      })
+    })
+  })
+
+  test.describe('Con aria snapshot', () => {
+    operaciones.forEach(async ({ operacion, nombre }) => {
+      test(`aria snapshot ${nombre}`, async ({ page }) => {
+        await page.getByRole('button', { name: '5' }).click();
+        await page.getByRole('button', { name: operacion }).click();
+        await expect(page.getByRole('main')).toMatchAriaSnapshot(`
+          - main:
+            - heading "Calculadora" [level=1]
+            - status: 5 ${operacion}
+            - status: "5"
+            - button "C"
+            - button "↶ BORRAR"
+            - button "+"
+            - button "7"
+            - button "8"
+            - button "9"
+            - button "-"
+            - button "4"
+            - button "5"
+            - button "6"
+            - button "*"
+            - button "1"
+            - button "2"
+            - button "3"
+            - button //
+            - button "±"
+            - button "0"
+            - button ","
+            - button "="
+          `);
+      })
+    })
+  })
+})
--- tests/contactos.spec.ts
+++ tests/contactos.spec.ts
@@ -29,7 +29,12 @@ test('prueba la paginación de los contactos', { tag: '@smoke' }, async ({ page 
   await page.getByRole('link').filter({ hasText: /^inicio$/ }).click();
   await expect(page.getByText('Dr. Adolf Dunster Tlfn.: 699')).toBeVisible();
 });
-test('formulario', { tag: '@smoke' }, async ({ page }) => {
+
+test('consulta un contacto', async ({ page, request }) => {
+  
+});
+
+test('añadir un contacto', { tag: '@smoke' }, async ({ page }) => {
   await page.getByRole('button', { name: 'Añadir' }).describe('pulsar el botón de añadir').click();
   await page.getByRole('spinbutton', { name: 'Código:' }).describe('meter el código incorrecto').fill('-1');
   await page.getByRole('spinbutton', { name: 'Código:' }).press('Tab');
@@ -45,8 +50,91 @@ test('formulario', { tag: '@smoke' }, async ({ page }) => {
   await page.getByRole('radio', { name: 'Mujer' }).check();
   await page.getByRole('checkbox', { name: 'Conflictivo' }).check();
   await page.getByRole('textbox', { name: 'Avatar:' }).fill('https://randomuser.me/api/portraits/women/1.jpg');
-  // await page.getByRole('button', { name: 'Enviar' }).last().click();
   await btnEnviar.click();
- 
-  // await page.locator('#frmPrincipal').click();
+});
+
+test('añadir un mal contacto', async ({ page }) => {
+  await page.getByRole('button', { name: 'Añadir' }).click();
+  await btnEnviar.click();
+  await expect(page.locator('#err_id')).toBeVisible();
+  await expect(page.locator('#err_id')).toContainText('Completa este campo');
+  await expect(page.locator('#err_nombre')).toBeVisible();
+  await expect(page.locator('#err_nombre')).toContainText('Completa este campo');
+
+  await page.getByRole('spinbutton', { name: 'Código:' }).fill('-1');
+  await page.getByRole('spinbutton', { name: 'Código:' }).press('Tab');
+  await expect(page.locator('#err_id')).toBeVisible();
+  await expect(page.locator('#err_id')).toContainText('El valor debe ser superior o igual a 0');
+  await page.getByRole('spinbutton', { name: 'Código:' }).fill('0');
+  await page.getByRole('spinbutton', { name: 'Código:' }).press('Tab');
+  await expect(page.locator('#err_id')).toBeHidden();
+
+  await page.getByRole('textbox', { name: 'Nombre:' }).fill('1');
+  await page.getByRole('textbox', { name: 'Apellidos:' }).fill('1');
+  await page.getByRole('textbox', { name: 'Correo:' }).fill('1');
+  await page.getByRole('textbox', { name: 'F. Nacimiento:' }).pressSequentially('310');
+  await page.getByRole('textbox', { name: 'F. Nacimiento:' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Avatar:' }).fill('1');
+
+  await page.getByRole('button', { name: 'Volver' }).focus();
+
+  await expect(page.locator('#err_nombre')).toContainText('Aumenta la longitud de este texto a 2 caracteres o más (actualmente, el texto tiene 1 carácter).');
+  await expect(page.locator('#err_apellidos')).toContainText('Aumenta la longitud de este texto a 2 caracteres o más (actualmente, el texto tiene 1 carácter).');
+  await expect(page.locator('#err_email')).toContainText('Incluye un signo "@" en la dirección de correo electrónico. La dirección "1" no incluye el signo "@".');
+  await expect(page.locator('#err_avatar')).toContainText('Introduce una URL');
+
+  // await expect(page.getByRole('main')).toMatchAriaSnapshot(`
+  //   - text: Datos inválidos
+  //   - button
+  //   - heading "Contactos" [level=1]
+  //   - text: "Código:"
+  //   - spinbutton "Código:"
+  //   - text: "Tratamiento:"
+  //   - text: "Nombre:"
+  //   - textbox "Nombre:"
+  //   - text: "Aumenta la longitud de este texto a 2 caracteres o más (actualmente, el texto tiene 1 carácter). Apellidos:"
+  //   - textbox "Apellidos:"
+  //   - text: "Aumenta la longitud de este texto a 2 caracteres o más (actualmente, el texto tiene 1 carácter). Teléfono:"
+  //   - textbox "Teléfono:"
+  //   - text: "Correo:"
+  //   - textbox "Correo:"
+  //   - text: "Incluye un signo \\"@\\" en la dirección de correo electrónico. La dirección \\"1\\" no incluye el signo \\"@\\". F. Nacimiento:"
+  //   - text: "Conflictivo Avatar:"
+  //   - textbox "Avatar:"
+  //   - text: Introduce una URL
+  //   - button "Enviar" [disabled]
+  //   - button "Volver"
+  //   `);
+
+  // await page.getByRole('textbox', { name: 'Nombre:' }).fill('12');
+  // await page.getByRole('textbox', { name: 'Apellidos:' }).fill('12');
+  // await page.getByRole('textbox', { name: 'Correo:' }).fill('a@a');
+  // await page.getByRole('textbox', { name: 'Avatar:' }).fill('http://www.example.com/image.png');
+
+  await page.getByRole('button', { name: 'Volver' }).focus();
+});
+
+test('añadir un mal contacto - con instantáneas', async ({ page }) => {
+  await page.getByRole('button', { name: 'Añadir' }).click();
+  await btnEnviar.click();
+  await expect(page.getByRole('main')).toHaveScreenshot('inicial.png');
+
+  await page.getByRole('spinbutton', { name: 'Código:' }).fill('-1');
+  await page.getByRole('textbox', { name: 'Nombre:' }).fill('1');
+  await page.getByRole('textbox', { name: 'Apellidos:' }).fill('1');
+  await page.getByRole('textbox', { name: 'Correo:' }).fill('1');
+  await page.getByRole('textbox', { name: 'F. Nacimiento:' }).pressSequentially('310');
+  await page.getByRole('textbox', { name: 'F. Nacimiento:' }).press('Tab');
+  await page.getByRole('textbox', { name: 'Avatar:' }).fill('1');
+  await page.getByRole('button', { name: 'Volver' }).focus();
+  await expect(page.getByRole('main')).toHaveScreenshot('con-errores.png');
+
+  await page.getByRole('spinbutton', { name: 'Código:' }).fill('0');
+  await page.getByRole('textbox', { name: 'Nombre:' }).fill('12');
+  await page.getByRole('textbox', { name: 'Apellidos:' }).fill('12');
+  await page.getByRole('textbox', { name: 'Correo:' }).fill('a@a');
+  await page.getByRole('textbox', { name: 'F. Nacimiento:' }).fill('2008-09-28');
+  await page.getByRole('textbox', { name: 'Avatar:' }).fill('http://www.example.com/image.png');
+  await page.getByRole('button', { name: 'Volver' }).focus();
+  await expect(page.getByRole('main')).toHaveScreenshot('sin-errores.png');
 });
